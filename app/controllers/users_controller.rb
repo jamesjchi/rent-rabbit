850c2cd312d59d20043a96bf900065ed0612e803
egervais7 <edward.a.gervais89@gmail.com>
2016-01-11 15:28:01 -0800

    got user profile page to render user information, user items, and ability to edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,27 @@ class UsersController < ApplicationController
   def create
   end
 
-  def update
+  def edit
+    @user = User.find params[:id]
+    @user.update(user_params)
+    redirect_to show_users_path
   end
 
-  def edit
+  def update
+
   end
 
   def show
+    @user = User.find params[:id]
+    @item = @user.item
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :image, :location, :bio)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,8 @@ Rails.application.routes.draw do
   resources :items
   root 'main#index'
 
+  post '/users' => 'users#update'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
