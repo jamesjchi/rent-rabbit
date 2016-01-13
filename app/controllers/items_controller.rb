9d44d0696d42150a28918c3d91b23cbf14a7530c
jamesjchi <jamesjchi@gmail.com>
2016-01-13 14:46:53 -0800

    added cloudinary upload

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,12 +13,20 @@ class ItemsController < ApplicationController
     end
     if @item.save
       flash[:success] = "Item Created"
-      redirect_to items_path
+      redirect_to @current_user
     else
       messages = @item.errors.map { |k, v| "#{k} #{v}" }
       flash[:danger] = messages.join(', ').gsub!(/_/, ' ')
       redirect_to new_item_path
     end
+
+    uploaded = params[:item][:image].path
+    cloud_file = Cloudinary::Uploader.upload(uploaded)
+    if (File.exists?(uploaded))
+      File.delete(uploaded)
+    end
+    @item.update({:image => cloud_file["public_id"]})
+
   end
 
   def new
@@ -36,6 +44,15 @@ class ItemsController < ApplicationController
   def update
     item = Item.find params[:id]
     item.update item_params
+    # Update image, if item has an image
+    if params[:item][:image]
+      uploaded_image = params[:item][:image].path
+      cloud_file = Cloudinary::Uploader.upload(uploaded_image)
+      if (File.exists?(uploaded_image))
+      File.delete(uploaded_image)
+      end
+      item.update({:image => cloud_file["public_id"]})
+    end
     redirect_to items_path
   end
 
@@ -47,6 +64,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :description, :price_day, :price_week, :start_date, :end_date, :rented)
+    params.require(:item).permit(:title, :description, :price_day, :price_week, :start_date, :end_date, :image, :rented)
   end
 end
