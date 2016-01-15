60b97624ae008674899bde2cee9a057df155c4e8
egervais7 <edward.a.gervais89@gmail.com>
2016-01-14 23:07:24 -0800

    added require 'net/http' to get captcha to work on heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'https://rent-rabbit.herokuapp.com',
+    domain:               'https://rent-rabbit.herokuapp.com/',
     user_name:            'rent.rabbit.dev.team',
     password:             'rentrabbitdevteam',
     authentication:       'cram_md5',
